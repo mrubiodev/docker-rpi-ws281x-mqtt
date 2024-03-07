a634ee8e79ac4f4510367c97c1ee9f08cf98ab7a
it-guy92 <18345556+it-guy92@users.noreply.github.com>
2024-03-07 16:13:30 +0100

    testing

--- effects/solid.py
+++ effects/solid.py
@@ -4,14 +4,24 @@
 import time
 from effects.utils import utils
 
+LED_COUNT = 0
+
+def init(strip):
+    global LED_COUNT
+    LED_COUNT = strip.numPixels()
 
 def effect_solid(strip, color, brightness):
     utils.set_all_leds_color(
         strip, utils.get_color(color, brightness))
     time.sleep(10)
 
-#einzelnes segment ansteuern
+#einzelnes segment ansteuern 
+#die funktion soll dabei den gesamtzustand des Lightstrips speichern damit die nicht veränderten LEDS nicht verändert werden
+    
+
+
 def effect_solid_segment(strip, color, brightness, start, end):
+    print(LED_COUNT)
     utils.set_segment_color(
         strip, utils.get_color(color, brightness), start, end)
     time.sleep(10)
