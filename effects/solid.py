af529d2001fecee510e0d166ccce6f60410ac670
it-guy92 <18345556+it-guy92@users.noreply.github.com>
2024-03-07 10:40:37 +0100

    Update solid.py

--- effects/solid.py
+++ effects/solid.py
@@ -9,3 +9,9 @@ def effect_solid(strip, color, brightness):
     utils.set_all_leds_color(
         strip, utils.get_color(color, brightness))
     time.sleep(10)
+
+#einzelnes segment ansteuern
+def effect_solid_segment(strip, color, brightness, start, end):
+    utils.set_segment_color(
+        strip, utils.get_color(color, brightness), start, end)
+    time.sleep(10)
